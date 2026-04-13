f753f1e35c3346f3e5fadd42f7e22ea9d582e10e
Turab Manzoor <turabmanzoor032@gmail.com>
2026-04-13 12:41:44 +0530

    added employee

--- mvcapp/Controllers/DashboardController.cs
+++ mvcapp/Controllers/DashboardController.cs
@@ -5,17 +5,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using mvcapp.Models;
 
 namespace mvcapp.Controllers
 {
 
-    public class DashboardController : Controller
+    public class DashboardController(AppDbContext _context) : Controller
     {
 
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        public IActionResult RedirectToEmployeeForm()
+        {
+            return RedirectToAction("AddEmployee");
         }
 
 
